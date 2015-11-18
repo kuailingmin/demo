f1a83d76bed7109642871873c126b45eb9f956c7
star <kuailingmin@126.com>
2015-11-18 14:20:43 +0800

    star -- typescript创建类操作

--- calsses.js
+++ calsses.js
@@ -1,4 +1,17 @@
 /**
  * Created by star on 15/11/18.
  */
+//创建类
+var Greeter = (function () {
+    function Greeter(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    Greeter.prototype.print = function () {
+        return "姓名：" + this.name + "--年龄：" + this.age;
+    };
+    return Greeter;
+})();
+var p = new Greeter("star", 10);
+console.log(p.print());
 //# sourceMappingURL=calsses.js.map
--- calsses.ts
+++ calsses.ts
@@ -1,3 +1,20 @@
 /**
  * Created by star on 15/11/18.
  */
+
+//创建类
+class Greeter{
+    name:String;
+    age:number;
+    constructor(name:string,age:number){
+        this.name = name;
+        this.age = age;
+    }
+    print(){
+        return "姓名：" + this.name + "--年龄：" +this.age;
+    }
+}
+
+var p = new Greeter("star",10);
+console.log(p.print());
+
