9179bb029f764a6f3d72653794908d9fe6665ee4
star <kuailingmin@126.com>
2015-11-18 13:28:34 +0800

    star -- typescript自学

---
+++ boolean.js
@@ -0,0 +1,9 @@
+/**
+ * Created by star on 15/11/18.
+ */
+var isBoon = false;
+function star() {
+    console.log(isBoon);
+}
+star();
+//# sourceMappingURL=boolean.js.map
---
+++ number.js
@@ -0,0 +1,9 @@
+/**
+ * Created by star on 15/11/18.
+ */
+var num = 10;
+function tell() {
+    console.log(num);
+}
+tell();
+//# sourceMappingURL=number.js.map
