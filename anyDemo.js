cf4554f7d130c5ab76e4150de9b4a3ae4c33cebe
star <kuailingmin@126.com>
2015-11-18 13:57:09 +0800

    star -- typescript自学

--- anyDemo.js
+++ anyDemo.js
@@ -1,4 +1,12 @@
 /**
  * Created by star on 15/11/18.
  */
+var anyVal = 10;
+var anyVal1 = true;
+function demo() {
+    return '';
+}
+function number() {
+    return 1;
+}
 //# sourceMappingURL=anyDemo.js.map
--- anyDemo.ts
+++ anyDemo.ts
@@ -1,3 +1,13 @@
 /**
  * Created by star on 15/11/18.
  */
+var anyVal:any = 10;
+var anyVal1:any = true;
+
+function demo():string{
+    return '';
+}
+
+function number():number{
+    return 1;
+}
