d866c15d6897e1d9fa4f2c8072a0f273208f489b
star <kuailingmin@126.com>
2015-11-18 13:28:04 +0800

    star -- typescript module定义

---
+++ boolean.ts
@@ -0,0 +1,9 @@
+/**
+ * Created by star on 15/11/18.
+ */
+var isBoon:boolean = false;
+
+function star(){
+    console.log(isBoon);
+}
+star();
---
+++ number.ts
@@ -0,0 +1,10 @@
+/**
+ * Created by star on 15/11/18.
+ */
+var num:number = 10;
+
+function tell(){
+    console.log(num);
+}
+
+tell();
